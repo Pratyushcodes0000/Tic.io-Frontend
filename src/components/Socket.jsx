e6b9d0ae38c9e25b695fdeb54c0f14632d83cc33
Pratyushcodes0000 <chowdhurypratyush24@gmail.com>
2025-11-23 21:38:48 +0530

    /

--- src/components/Socket.jsx
+++ src/components/Socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://tic-io-backend.vercel.app/", {
+const socket = io("https://tic-io-backend.vercel.app", {
   transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 5,
