660e9fd4ea2ffe22ea64f84236b4e7ebeb46d4c9
Pratyushcodes0000 <chowdhurypratyush24@gmail.com>
2025-11-23 21:48:55 +0530

    socket change

--- src/components/Socket.jsx
+++ src/components/Socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://tic-io-backend.vercel.app", {
+const socket = io("ticio-backend-production.up.railway.app", {
   transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 5,
